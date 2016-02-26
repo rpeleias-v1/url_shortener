9fce6c86a2dc20d581a0964538d28a4d4ae25d44
Rodrigo Peleias <rodrigo.peleias@preventsenior.com.br>
2016-02-26 15:39:01 -0300

    Prepared API documentation with apidoc

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,11 +8,35 @@ module.exports = function(app) {
 	var UrlUtils = app.utils.urlUtils;
 	var urlUtils = new UrlUtils();
 
+	/**
+	 * @api {get} / API index
+	 * @apiGroup URL Shortener API
+	 * @apiSuccess {String} status: API status message
+	 * @apiSuccessExample {json} Success
+	 *  HTTP/1.1 200 OK
+	 * {"status": "URL Shortener API"}
+	 */
 	app.route('/')
 	  .get(function(req, res) {
 	  	res.json({status: 'URL Shortener API'});
 	  });
 
+	/**
+	 * @api {get} /new/:url(*) Web site url to be reduced
+	 * @apiGroup URL Shortener API
+	 * @apiSuccess {String} original_url Original URL informed as path param
+	 * @apiSuccess {String} short_url Short URL made by API	 
+	 * @apiSuccessExample {json} Success
+	 *  HTTP/1.1 200 OK
+	 *  {
+	 *	  "original_url": "http://www.uol.com.br",
+	 *	  "short_url": "http://fast-sierra-77824.herokuapp.com/0yS7w" 
+	 *  } 
+	 * @apiErrorExample {json} Error-Response: 
+	 *  {
+	 *	  "error": 'Invalid URL'	 
+	 *  } 
+	 */
 	app.route('/new/:url(*)')	
 	  .get(function(req, res) {
 	  	var url = req.params.url;	  	  	
@@ -31,11 +55,20 @@ module.exports = function(app) {
 	  		if (req.query.allow === 'true') {
 	  			findOrCreate(urlUtils.removeHttpFromHost(url), req, res);
 	  		} else {
-	  			res.json({error: 'URL invalid'});
+	  			res.json({error: 'Invalid URL'});
 	  		}	  		
 	  	}	  	
 	  });
 
+	/**
+	 * @api {get} /:urlCode URL short code to be redirected 
+	 * @apiGroup URL Shortener API
+	 * @apiSuccess {String} original URL web site redirected	
+	 * @apiErrorExample {json} Error-Response:
+	 *  {
+	 *	  "error": 'URL not found'	 
+	 *  } 
+	 */
 	app.route('/:urlCode')
 		.get(function(req, res) {			
 			UrlShortener.findOne({
